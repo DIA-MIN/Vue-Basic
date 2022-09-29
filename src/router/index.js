6570810bdd55de59d3b86ed79d4d2918e2c52711
unknown <mns0327@naver.com>
2022-09-29 16:52:26 +0900

    :document::sparkles: add prettierrc(Prettier 커스텀 규칙 doc) & Router 방법 3가지

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,18 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView // 방법1. => 첫 렌딩 페이지를 띄울 컴포넌트 같은 경우 사용
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // 방법2. => size가 작으면서 사용자가 컴포넌트 요청을 안할 수도 있는 경우.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // webpackChunkName 지정 이름으로 컴포넌트 파일 명칭이 바뀜 (개발자 도구 네트워크 탭)
+
+    // 방법3. => size가 크면서 사용자가 컴포넌트 사용할 가능성이 큰 경우. (webpackPrefetch)
+    // webpackPrefetch: true => 브라우저 캐시에 저장하겠다. 확실히 사용할 컴포넌트이면서, 파일 크기가 클 경우만 사용하기!
   }
 ]
 
