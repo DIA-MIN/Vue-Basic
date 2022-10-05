35ac942607a20fff5820afb820191069166bd2b4
unknown <mns0327@naver.com>
2022-10-05 14:10:45 +0900

    :sparkles: databinding string type

---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hellow {{ userName }}</h1>
+    <!-- 문자열 databinding -> {{}} -->
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: 'min.gr8',
+      msg: "i'm G-IDLE illustrator :)",
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
+
+<style scoped></style>
