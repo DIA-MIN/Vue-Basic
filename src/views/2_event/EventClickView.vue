605598022cdb5e3e2c5875d0abb35bac70d54b0b
unknown <mns0327@naver.com>
2022-10-06 15:53:28 +0900

    :sparkles: add event us click (@click)

---
+++ src/views/2_event/EventClickView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="increase">Add 1</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    increase() {
+      this.counter++
+    }
+  }
+}
+</script>
