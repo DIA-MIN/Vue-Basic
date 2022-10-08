8236a50a0fb1e826561a250bb046d7a1bf9812e1
unknown <mns0327@naver.com>
2022-10-08 23:49:02 +0900

    add Vue Toggle Example :)

--- src/views/2_event/EventClickView.vue
+++ src/views/2_event/EventClickView.vue
@@ -2,6 +2,16 @@
   <div>
     <button @click="increase">Add 1</button>
     <p>{{ counter }}</p>
+    <ul class="city-buttons">
+      <il
+        @click="selectCity(city)"
+        class="city-button"
+        :class="{ isSelected: city.selected === true }"
+        v-for="city in cities"
+        :key="city.code"
+        >{{ city.label }}</il
+      >
+    </ul>
   </div>
 </template>
 <script>
@@ -9,7 +19,12 @@ export default {
   components: {},
   data() {
     return {
-      counter: 0
+      counter: 0,
+      cities: [
+        { code: 'Seoul', label: '서울', selected: false },
+        { code: 'London', label: '런던', selected: false },
+        { code: 'Chicago', label: '시카고', selected: false }
+      ]
     }
   },
   setup() {},
@@ -19,7 +34,23 @@ export default {
   methods: {
     increase() {
       this.counter++
+    },
+    selectCity(city) {
+      city.selected = !city.selected
     }
   }
 }
 </script>
+<style>
+.city-buttons {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+.city-button {
+  cursor: pointer;
+}
+.isSelected {
+  background: #333;
+  color: #fff;
+}
+</style>
